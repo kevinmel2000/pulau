07e79cbce5f81368d4b3e124b2f203cbcc72a89f
Andy Librian <andylibrian@gmail.com>
2015-04-18 19:03:04 +0700

    provinsi single resource

--- src/main/java/com/andylibrian/pulau/resources/ProvinsiResource.java
+++ src/main/java/com/andylibrian/pulau/resources/ProvinsiResource.java
@@ -3,11 +3,13 @@ package com.andylibrian.pulau.resources;
 import com.andylibrian.pulau.core.Provinsi;
 import com.andylibrian.pulau.core.ProvinsiRepository;
 import com.codahale.metrics.annotation.Timed;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,18 +19,35 @@ public class ProvinsiResource {
 
     private final Map<String, Object> documentAllProvinsi;
     private final List<Provinsi> provinsiList;
+    private final HashMap<String, Provinsi> provinsiMap;
 
     public ProvinsiResource() {
         final ProvinsiRepository repo = new ProvinsiRepository();
         provinsiList = repo.findAll();
+        
+        provinsiMap = new HashMap<String, Provinsi>();
+        for (Provinsi p : provinsiList) {
+            provinsiMap.put(p.getId(), p);
+        }
 
         documentAllProvinsi = new LinkedHashMap<String, Object>();
         documentAllProvinsi.put("data", provinsiList);
     }
 
     @GET
-    @Timed
     public Map<String, Object> findAll() {
         return documentAllProvinsi;
+    }
+
+    @GET @Path("/{id}")
+    public Map<String, Object> findSingle(@PathParam("id") String id) {
+        
+        Provinsi provinsi = provinsiMap.get(id);
+
+        final Map<String, Object> documentSingleProvinsi = new LinkedHashMap<String, Object>();
+        documentSingleProvinsi.put("data", provinsi);
+        
+        return documentSingleProvinsi;
+                
     }
 }
